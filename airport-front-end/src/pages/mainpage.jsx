0034dbc84b88e08225de51549c38ea352c03873c
Marc00900 <marcgasensio@icloud.com>
2025-04-21 15:37:46 +0200

    añadir boton logout a mainpage

--- airport-front-end/src/pages/mainpage.jsx
+++ airport-front-end/src/pages/mainpage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '/src/pages/css/mainpage.css';
+import LogOutButton from "/src/components/LogOutButton.jsx";
 import logo from "/src/pages/images/LogoBlanco.png";
 import coche from "/src/pages/images/coche.png";
 import avion from "/src/pages/images/avion.png";
@@ -22,9 +23,7 @@ function Header() {
         <button title="Perfil">
           <img src={perfil} alt="Perfil" />
         </button>
-        <button title="Sortir">
-          <img src={sortir} alt="Sortir" />
-        </button>
+        <LogOutButton></LogOutButton>
       </div>
     </header>
   );
