4888ff59791114434fc2741d02c3056683383b28
San_2111 <96992641+Sk0404@users.noreply.github.com>
2025-05-17 15:03:02 +0200

    Update vite.config.js
    forgot a "

--- airport-front-end/vite.config.js
+++ airport-front-end/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        allowedHosts: ["projectevia-a.duckdns.org", flysy.software"]
+        allowedHosts: ["projectevia-a.duckdns.org", "flysy.software"]
     }
 });
