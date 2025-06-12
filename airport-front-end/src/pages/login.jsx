806a57fbfdc5e4014582ab5adaca4c06a9574cea
Daniel <danielferp2004@gmail.com>
2025-06-12 20:28:48 +0200

    S

--- airport-front-end/src/pages/login.jsx
+++ airport-front-end/src/pages/login.jsx
@@ -81,20 +81,6 @@ function Login() {
             //const userData = jwtDecode(credentialResponse.credential)
             console.log("Email del usuario:", userData.email);
             console.log("Nombre del usuario:", userData.name);
-            console.log("Email del usuario:", userData.email);
-            console.log("Nombre del usuario:", userData.name);
-            console.log("Email del usuario:", userData.email);
-            console.log("Nombre del usuario:", userData.name);
-            console.log("Email del usuario:", userData.email);
-            console.log("Nombre del usuario:", userData.name);
-            console.log("Email del usuario:", userData.email);
-            console.log("Nombre del usuario:", userData.name);
-            console.log("Email del usuario:", userData.email);
-            console.log("Nombre del usuario:", userData.name);
-            console.log("Email del usuario:", userData.email);
-            console.log("Nombre del usuario:", userData.name);
-            console.log("Email del usuario:", userData.email);
-            console.log("Nombre del usuario:", userData.name);
             
             const registerRes = await fetch("https://flysy.software/api/register-login-google", {
               method: "POST",
